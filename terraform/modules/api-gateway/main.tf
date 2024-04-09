2f9ff6a39927092bcd44e6370d3802ae2414841f
Chris Lee <>
2024-04-09 15:21:26 +0800

    Support Name Card

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -18,17 +18,17 @@ resource "aws_api_gateway_resource" "scan" {
   path_part   = "scan"
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "post_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.scan.id
-  http_method   = "POST" # Adjust the HTTP method if needed
+  http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.scan.id
-  http_method             = aws_api_gateway_method.method.http_method
+  http_method             = aws_api_gateway_method.post_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.lambda.invoke_arn
@@ -37,7 +37,7 @@ resource "aws_api_gateway_integration" "integration" {
 resource "aws_api_gateway_method_response" "method_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.scan.id
-  http_method = aws_api_gateway_method.method.http_method
+  http_method = aws_api_gateway_method.post_method.http_method
   status_code = "200"
 
   response_models = {
